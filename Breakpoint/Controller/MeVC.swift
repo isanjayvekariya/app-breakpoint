2d3b9e5410975abe10585956db86c6710032c3ee
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-04 14:27:29 -0400

    create mevc interface added in tabbar

---
+++ Breakpoint/Controller/MeVC.swift
@@ -0,0 +1,23 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by Sanjay Vekariya on 9/4/17.
+//  Copyright © 2017 Sanjay Vekariya. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableview: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+    }
+}
