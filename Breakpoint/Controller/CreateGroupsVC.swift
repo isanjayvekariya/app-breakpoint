57f1e57ef456fd0b131aa50589ec8337918fc847
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-22 22:24:40 -0400

    create group vc UI stuff/outlets

--- Breakpoint/Controller/CreateGroupsVC.swift
+++ Breakpoint/Controller/CreateGroupsVC.swift
@@ -10,26 +10,24 @@ import UIKit
 
 class CreateGroupsVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var titleTxtField: InsetTextField!
+    @IBOutlet weak var descriptionTxtField: InsetTextField!
+    @IBOutlet weak var emailSearchTxtField: InsetTextField!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneBtnPressed(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func closeBtnPressed(_ sender: Any) {
+    }
+    
 }
