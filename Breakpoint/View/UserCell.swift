172080e7f054f22f963484babcd36e6f43dfdeca
Sanjay Vekariya <sanjayvekariya@Sanjays-MacBook-Pro.local>
2017-09-23 13:48:09 -0400

    adding users to group, didselect tablview

--- Breakpoint/View/UserCell.swift
+++ Breakpoint/View/UserCell.swift
@@ -14,20 +14,31 @@ class UserCell: UITableViewCell {
     @IBOutlet weak var emailLbl: UILabel!
     @IBOutlet weak var checkImg: UIImageView!
     
+    var showing = false
+    
     func configureCell(profileImage image: UIImage, email: String, isSelected: Bool ) {
         self.profileImg.image = image
         self.emailLbl.text = email
         
         if isSelected {
             self.checkImg.isHidden = false
+            showing = true
         } else {
             self.checkImg.isHidden = true
+            showing = false
         }
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        if selected {
+            if showing == false {
+                self.checkImg.isHidden = false
+                showing = true
+            }   else {
+                self.checkImg.isHidden = true
+                showing = false
+            }
+        }
     }
-
 }
